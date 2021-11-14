27ba67957a4be796899bedb07c2a04845c8fdf6a
Jarek Majka <vessekheal@gmail.com>
2021-11-14 12:24:25 +0100

    settings_local updated

--- medyczna_placowka/settings_local.py
+++ medyczna_placowka/settings_local.py
@@ -1,10 +1,6 @@
 from decouple import config
 
 DEBUG = True
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-EMAIL_HOST = config('EMAIL_HOST', default='localhost')
-EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
 
 DATABASES = {
     'default': {
