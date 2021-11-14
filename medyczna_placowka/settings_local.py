bfd6aeb8aae979baf2eef55032f67a6a8c989078
Jarek Majka <vessekheal@gmail.com>
2021-11-14 12:20:59 +0100

    settings_local created

---
+++ medyczna_placowka/settings_local.py
@@ -0,0 +1,18 @@
+from decouple import config
+
+DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
