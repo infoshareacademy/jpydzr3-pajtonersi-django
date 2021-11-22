b11c9f0cd5d8bce902a32e4b3ffc8792a7d56ca1
Jarek Majka <vessekheal@gmail.com>
2021-11-22 22:04:11 +0100

    Default values included

--- medyczna_placowka/settings_local.py
+++ medyczna_placowka/settings_local.py
@@ -5,10 +5,10 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME', default=''),
-        'USER': config('DB_USER', default=''),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default=''),
-        'PORT': config('DB_PORT', default=''),
+        'NAME': config('DB_NAME', default='DEFAULTDBNAME'),
+        'USER': config('DB_USER', default='DEFAULTUSERNAME'),
+        'PASSWORD': config('DB_PASSWORD', default='DEFAULTPASSWORD'),
+        'HOST': config('DB_HOST', default='DEFAULTHOST'),
+        'PORT': config('DB_PORT', default='1111'),
     }
 }
