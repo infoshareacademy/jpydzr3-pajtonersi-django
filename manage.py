c3a6a5d41cc8f621d1a4b19579c4ee124f98a385
Jarek Majka <vessekheal@gmail.com>
2021-11-23 20:11:17 +0100

    manage.py default path removed

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from decouple import config
 def main():
     """Run administrative tasks."""
     # TODO Create loop for different scenarios (prod, dev, local)
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('SETTINGS_PATH', default='medyczna_placowka.settings'))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('SETTINGS_PATH'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
