289eab13c153a5ecd0e68f56d03e47c6f6a39022
Jarek Majka <vessekheal@gmail.com>
2021-12-12 11:40:32 +0100

    Models created

--- users/models.py
+++ users/models.py
@@ -1,17 +1,37 @@
 from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext as _
+from django.db import models
 
-USER = get_user_model()
+User = get_user_model()
 
 
-class Receptionist(USER):
+class PersonalInfoMixin(models.Model):
+    pesel = models.IntegerField(max_length=11)
+    tel_no = models.CharField(max_length=20)
+
+    class Meta:
+        abstract = True
+
+
+class Address(models.Model):
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    street_name = models.CharField(max_length=50, help_text="Podaj z numer budynku")
+    local_no = models.IntegerField(blank=True, null=True)
+    post_code = models.CharField(max_length=20)
+
+
+class Receptionist(User, PersonalInfoMixin):
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
     class Meta:
         verbose_name = _('recepcjonista')
 
 
-class Patient(USER):
-    pass
+class Patient(User):
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
 
 
-class Doctor(USER):
-    pass
+class Doctor(User):
+    specialty = models.CharField(max_length=255)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
