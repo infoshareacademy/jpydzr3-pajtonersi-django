926a0ec29f8f3cd12973ee780146e9db922aa31e
Jarek Majka <vessekheal@gmail.com>
2021-12-12 13:01:46 +0100

    create_view test created

--- users/tests.py
+++ users/tests.py
@@ -1,11 +1,11 @@
 from django.test import TestCase, Client
 from django.urls import reverse_lazy
-from users.test_samples import variable_samples
+from http import HTTPStatus
 
 
 class UserCreateTestCase(TestCase):
     def test_create_view(self):
         client = Client()
         response = client.get(reverse_lazy('patient_create'))
-        expected = variable_samples.user_create_get
-        self.assertEqual(expected, response.rendered_content)
+        expected = HTTPStatus.OK
+        self.assertEqual(expected, response.status_code)
