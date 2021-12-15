f33b7815d9aab06b977af2283de5e96b19f64248
JanLuczewski <jan.luczewski@gmail.com>
2021-12-15 17:35:35 +0100

    widok gowny

--- medyczna_placowka/urls.py
+++ medyczna_placowka/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-]
+    path('', TemplateView.as_view(template_name='index.html')),
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
