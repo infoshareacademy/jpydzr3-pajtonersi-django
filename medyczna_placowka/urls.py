966e3a2694dc234826b091816113b2fa44d01b07
Karol Mularski <karolmularski94@gmail.com>
2022-01-09 13:43:52 +0100

    add url

--- medyczna_placowka/urls.py
+++ medyczna_placowka/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
+    path('', include('med_doc.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
