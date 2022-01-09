2c440a04c440aba55ebcdaf22f977fa89c57ddc3
Karol Mularski <karolmularski94@gmail.com>
2022-01-09 14:01:44 +0100

    add create visit view

---
+++ med_doc/forms.py
@@ -0,0 +1,13 @@
+from django.db.models.base import Model
+from django.forms import ModelForm, fields
+
+
+from med_doc.models import Visit
+
+
+class VisitForm(ModelForm):
+    class Meta:
+        model = Visit
+        fields = ['visit_date', 'doctor', 'patient', 'created_by', 'addition_info']
+
+        # TODO: patient not visible if visit created by himself
--- med_doc/views.py
+++ med_doc/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 
+from med_doc.forms import VisitForm
+
+
+
 def create_visit(request):
-    pass
+    template_name = ''
+    if request.method == 'POST':
+        form = VisitForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, template_name, context={'form': VisitForm()})
+        return render(request, template_name, context={'form': VisitForm(request.POST)})
+
+    elif request.mthod == 'GET':
+        return render(request, template_name, context={'form': VisitForm()})
+    else:
+        raise Exception('Method not allowed')  #TODO: use proper error code
