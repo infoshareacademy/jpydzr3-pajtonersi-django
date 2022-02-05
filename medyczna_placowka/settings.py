cb7347832abf792319858a4dfdbed13b1f700494
JanLuczewski <89295358+JanLuczewski@users.noreply.github.com>
2022-02-05 12:15:17 +0100

    strona glowna (#5)
    * nowy commit
    
    * widok gowny
    
    * hejka
    
    * strona glowna
    
    Co-authored-by: Karol Mularski <karolmularski94@gmail.com>

--- medyczna_placowka/settings.py
+++ medyczna_placowka/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- medyczna_placowka/urls.py
+++ medyczna_placowka/urls.py
@@ -17,9 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('', include('users.urls')),
     path('', include('med_doc.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
