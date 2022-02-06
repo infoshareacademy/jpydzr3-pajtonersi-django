3a7068a9977fa151e87791146c9f5bdd4088acb2
Karol Mularski <karolmularski94@gmail.com>
2022-02-06 13:56:00 +0100

    add datetimepicker

--- med_doc/forms.py
+++ med_doc/forms.py
@@ -1,13 +1,30 @@
-from django.db.models.base import Model
-from django.forms import ModelForm, fields
-
+from django import forms
 
 from med_doc.models import Visit
 
 
-class VisitForm(ModelForm):
+class BootstrapDateTimePickerInput(forms.DateTimeInput):
+    template_name = 'widgets/bootstrap_datetimepicker.html'
+
+    def get_context(self, name, value, attrs):
+        datetimepicker_id = 'datetimepicker_{name}'.format(name=name)
+        if attrs is None:
+            attrs = dict()
+        attrs['data-target'] = '#{id}'.format(id=datetimepicker_id)
+        attrs['class'] = 'form-control datetimepicker-input'
+        context = super().get_context(name, value, attrs)
+        context['widget']['datetimepicker_id'] = datetimepicker_id
+        return context
+
+
+class VisitForm(forms.ModelForm):
+    visit_date = forms.DateTimeField(
+        input_formats=['%d/%m/%Y %H:%M'],
+        widget=BootstrapDateTimePickerInput()
+    )
+
     class Meta:
         model = Visit
-        fields = ['visit_date', 'doctor', 'patient', 'created_by', 'additional_info']
+        fields = ['doctor', 'patient', 'created_by', 'additional_info']
 
         # TODO: patient not visible if visit created by himself
